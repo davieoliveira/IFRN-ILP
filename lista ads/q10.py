fb18b01b0cacdc2fb1583919e82c3845f89480c8
davieoliveira <emanuel.davi@escolar.ifrn.edu.br>
2025-04-10 11:07:38 -0300

    adicionando questões da lista de algoritmos de ads

---
+++ lista ads/q10.py
@@ -0,0 +1,8 @@
+# Problema Sublista contígua de soma máxima.
+# Considere um array A de n números inteiros. Exemplo: A = [10, 5, −17, 20, 50, −1, 3, −30, 10]
+# O problema consiste em encontrar a maior subsequencia cuja soma seja a maior possível. No caso do vetor A a
+# maior soma é 72, que é a soma dos elementos entre os índices 3 e 6 : 20 + 50 + −1 + 3.
+# (20 + 50 -1 + 3 = 72) Maior soma possivel.
+
+a = [10, 5, -17, 20, 50, -1, 3, -30, 10]
+
